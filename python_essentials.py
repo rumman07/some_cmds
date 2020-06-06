8d7fc4d9fbd8e421d354dff078c830d60910fc52
rumman07 <rummanahmed.ra@gmail.com>
2020-06-06 16:54:56 -0500

    revision scripting languages

---
+++ python_essentials.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.8
+#Using slicing for Lists
+users = []
+users.append("john")
+users.append("linda")
+users.append("marry")
+users.append("aaron")
+users.append("blake")
+num = "123"
+#Slice the 1 and last from List
+print(users[2:1])
